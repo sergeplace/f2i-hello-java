50c089a76d1eac09d3d1657e72b69992c55dc2c1
serge <sergeplace@gmail.com>
2019-11-05 15:50:51 +0100

    up

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Jenkins Comment ça va ?");
+		System.out.println("Hello Jenkins, ceci est une update");
 	}
 }
